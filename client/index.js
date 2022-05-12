f2f590612adf43f4aeb6e27fd58990ce431736be
dsump <dsumpmann@gmail.com>
2022-05-12 11:16:40 -0600

    Basic client and server config with Apollo

--- client/index.js
+++ client/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({});
+// Apollo makes the assumption that the app.use() in server.js is routed to '/graphql'
+// see ../server/server.js line 31
+
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>Lyrical</div>
+    </ApolloProvider>
+  );
 };
+// Apollo Provider is basically the glue layer between the backend, the Apollo Store, and the front end
 
 ReactDOM.render(
   <Root />,
